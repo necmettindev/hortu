ae3efb2734c958aaee7883aa26c15bc098cef53c
necmettindev <necmettinayata@icloud.com>
2023-08-26 15:21:39 +0300

    feat(debounce.ts): add debounce function for rate-limiting tasks
    The `debounce.ts` file introduces a new `debounce` function that creates a debounced version of a provided function. This debounced function delays invoking the original function until after a specified number of milliseconds have elapsed since the last time the debounced function was invoked. It is useful for rate-limiting tasks like event handlers for user input events such as resize, scroll, and input.
    
    The `debounce` function accepts the following parameters:
    - `func`: The function to debounce.
    - `waitMs`: The number of milliseconds to delay the invocation of the original function (default is 50).
    - `options`: An optional configuration object with the following properties:
      - `isImmediate`: If `true`, the function will be invoked on the leading edge instead of the trailing edge.
      - `maxWait`: The maximum time the function is allowed to be delayed before it's invoked.
      - `callback`: An optional callback function that is invoked with the result of the original function after it is called.
    
    The debounced function returned by `debounce` has the following properties:
    - It can be invoked with any number of arguments.
    - It returns a promise that resolves with the result of the original function.
    - It has a `cancel` method that can be used to cancel any pending invocation of the original function.
    
    Example usage:
    ```typescript
    const debouncedSave = debounce(saveData, 200);
    debouncedSave(data);  // Will delay the invocation of `saveData` by 200ms.
    ```
    
    The `debounce` function improves the performance and responsiveness of event handlers by limiting the frequency of their execution, ensuring that they are only invoked after a certain delay has passed since the last invocation.

---
+++ library/src/function/debounce.ts
@@ -0,0 +1,95 @@
+export type DebounceOptions<Result> = {
+  isImmediate?: boolean;
+  maxWait?: number;
+  callback?: (data: Result) => void;
+};
+
+export interface DebouncedFunc<Args extends unknown[], Result> {
+  (this: any, ...args: Args): Promise<Result>;
+  cancel: (reason?: unknown) => void;
+}
+
+interface DebouncedPromise<FReturn> {
+  resolve: (result: FReturn) => void;
+  reject: (reason?: unknown) => void;
+}
+
+/**
+ * Creates a debounced version of the provided function. The debounced function delays invoking the
+ * original function until after `waitMs` milliseconds have elapsed since the last time the debounced
+ * function was invoked. Useful for rate-limiting tasks like event handlers of user input (e.g., resize, scroll, input).
+ *
+ * @param func - The function to debounce.
+ * @param waitMs - The number of milliseconds to delay (default is 50).
+ * @param options - An optional configuration object:
+ *    isImmediate: If `true`, the function will be invoked on the leading edge instead of the trailing edge.
+ *    maxWait: Maximum time func is allowed to be delayed before it's invoked.
+ *    callback: An optional callback function that is invoked with the result of `func` after `func` is called.
+ *
+ * @returns A debounced version of the provided function. This debounced function returns a promise
+ *          that resolves with the result of the original function. It also has a `cancel` method to
+ *          cancel any pending invocation of the original function.
+ *
+ * Example:
+ *
+ * const debouncedSave = debounce(saveData, 200);
+ * debouncedSave(data);  // Will delay the invocation of `saveData` by 200ms.
+ */
+export function debounce<Args extends unknown[], Result>(
+  func: (...args: Args) => Result,
+  waitMs: number = 50,
+  options: DebounceOptions<Result> = {}
+): DebouncedFunc<Args, Result> {
+  let timeoutId: ReturnType<typeof setTimeout> | undefined;
+  const { isImmediate = false, callback, maxWait } = options;
+  let lastInvokeTime = Date.now();
+  let promises: DebouncedPromise<Result>[] = [];
+
+  function nextInvokeTimeout(): number {
+    if (maxWait) {
+      const elapsed = Date.now() - lastInvokeTime;
+      return maxWait && elapsed + waitMs >= maxWait
+        ? maxWait - elapsed
+        : waitMs;
+    }
+    return waitMs;
+  }
+
+  const debounced: DebouncedFunc<Args, Result> = function (...args: Args) {
+    return new Promise<Result>((resolve, reject) => {
+      const invoke = () => {
+        timeoutId = undefined;
+        lastInvokeTime = Date.now();
+        const result = func(...args);
+        if (callback) {
+          callback(result);
+        }
+        promises.forEach((p) => p.resolve(result));
+      };
+
+      if (isImmediate && timeoutId === undefined) {
+        const result = func(...args);
+        if (callback) {
+          callback(result);
+        }
+        resolve(result);
+      } else {
+        promises.push({ resolve, reject });
+        if (timeoutId !== undefined) {
+          clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(invoke, nextInvokeTimeout());
+      }
+    });
+  };
+
+  debounced.cancel = (reason?: unknown) => {
+    if (timeoutId !== undefined) {
+      clearTimeout(timeoutId);
+    }
+    promises.forEach((p) => p.reject(reason));
+    promises = [];
+  };
+
+  return debounced;
+}
